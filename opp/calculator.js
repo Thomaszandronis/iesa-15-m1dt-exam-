77114a46db06bbbdf0f7bba830c59b8338dc79f3
unknown <mremki.billel@live.fr>
2015-03-25 14:02:42 +0100

    calculatrice et verification

--- opp/calculator.js
+++ opp/calculator.js
@@ -1,37 +1,23 @@
-var Calculator = new Array;
-var nombre = 
-for (var i=1; i<=n ; i++){
-}
-function add(Calculator) {
-var result = 0;
-for (var i = 0; i< Calculator.length ; i++){
-	result += parseFloat(Calculator[i]);
-}
-return result;
-}
-function minus(Calculator) {
-	var result = Calculator[0];
-	for (var i = 1; i< Calculator.length ; i++){
-		result -= parseInt(Calculator[i]);
-	}
-return result;
-}
-function multiply(Calculator) {
-	var result = Calculator[0];
-	for (var i = 1; i< Calculator.length ; i++){
-		result *= parseFloat(Calculator[i]);
-	}
-return result;
-}
-function divide(Calculator) {
-	var result = Calculator[0];
-	for (var i = 1; i< Calculator.length ; i++){
-		result /= parseFloat(Calculator[i]);
-	}
-return result;
-}
-		
-			
+function Calculator(){
+	this.result = 0;
+
+	this.add = function (a){  
+     this.result += ParseFloat(a); 
+    }
+
+    this.minus = function (a){  
+     this.result -= ParseFloat(a); 
+    }
+
+    this.multiply = function (a){  
+     this.result *= ParseFloat(a); 
+    }
+
+    this.divide = function (a){  
+     this.result /= ParseFloat(a); 
+    }
+}		
+
 		
 				
 		
